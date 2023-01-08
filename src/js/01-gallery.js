176f6098ccd2e9b6b248405a4236cb077564d6be
msselizabeth <lizagotovitryby@gmail.com>
2023-01-08 16:06:40 +0200

    parsel+src

---
+++ src/js/01-gallery.js
@@ -0,0 +1,51 @@
+// Add imports above this line
+// import { galleryItems } from './gallery-items';
+// Change code below this line
+
+// console.log(galleryItems);
+
+// const galleryContainer = document.querySelector('.gallery');
+// const galleryMarkup = createGalleryItems(galleryItems);
+
+// galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// galleryContainer.addEventListener('click', onGalleryItemClick);
+
+// function createGalleryItems (galleryItems) {
+//     return galleryItems
+//     .map(({ preview, original, description }) => {
+//     return `
+//     <div class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </div>`
+// }).join('');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
